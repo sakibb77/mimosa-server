881f1f42f4e2d80bf07417fdaad42b8d1537e9ab
sakib biswas <sakibbiswas418@mail.com>
2023-12-11 05:43:10 -0800

    registration method add

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, model } from 'mongoose';
 import { userType } from '../types/user.type';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema<userType>(
   {
@@ -44,6 +46,50 @@ const userSchema = new Schema<userType>(
     timestamps: true,
   }
 );
+
+userSchema.statics.register = async function (
+  name,
+  email,
+  password,
+  photoUrl,
+  address,
+  phoneNumber
+): Promise<userType> {
+  if (!name || !email || !password || !photoUrl) {
+    throw new Error('must fill name,email,password and photoUrl');
+  }
+
+  const existingUser = await this.findOne({ email });
+
+  if (existingUser) {
+    throw new Error('email already exist');
+  }
+
+  if (!validator.isEmail(email)) {
+    throw new Error('email already exist');
+  }
+
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      'Password must 8+ chars, contains uppercase, number and spacial chars'
+    );
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  const hash = await bcrypt.hash(password, salt);
+
+  const user = await this.create({
+    name,
+    email,
+    password: hash,
+    photoUrl,
+    address,
+    phoneNumber,
+  });
+
+  return user;
+};
 
 const userModel = model<userType>('User', userSchema);
 
